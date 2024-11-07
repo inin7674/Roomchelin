c959789a628ae6413527d40e7e1255456e8804a5
harintin <harintin@github.com>
2024-11-07 17:29:40 +0900

    feat: header setting

--- roomchelin/src/components/layout/header/Header.jsx
+++ roomchelin/src/components/layout/header/Header.jsx
@@ -1,56 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
+import '../../../assets/css/header.css';
 
 const Header = () => {
     return(
-        <HeaderWrapper>
-            <HeaderStyle as="div" className="header-inner">
-                <HeaderLogo as="h1" className="header-logo">
-                    <a to="/">
-                        <span>방슐랭</span>
-                    </a>
-                </HeaderLogo>
-            </HeaderStyle>
-        </HeaderWrapper>
+        <header className='header'>
+            <h1 className='header-logo'>
+                <a href="#">
+                    <span>방슐랭</span>
+                </a>
+            </h1>
+        </header>
     )
 }
-
-const HeaderStyle = styled.header`
-    div {
-        display: flex;
-        width: 250px;
-        height: 90px;
-    }
-    
-`;
-
-const HeaderLogo = styled.header`
-    #header-logo {
-        display: block;
-        transition: color 0.3s ease-in-out;
-        text-transform: uppercase;
-        text-align: center;
-        font-size: 26px;
-        color: var(--primary-dark);
-        span {
-            font-weight: 700;
-        }
-        &:hover {
-            color: var(--primary);
-        }
-    }
-`
-
-const HeaderWrapper = styled.header`
-    position: fixed;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    padding: 0 40px;
-    height: 120px;
-    box-sizing: border-box;
-    background-color: var(--white);
-`;
 
 export default Header;
